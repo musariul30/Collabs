619336a5bbaae91d5539a063fdf6439a2df8859b
arnellahayya <arnellahayya25@gmail.com>
2024-08-29 12:08:11 +0700

    Up Asssets top10

--- dummydata.js
+++ dummydata.js
@@ -5,7 +5,7 @@ const data_dummy = [
         collaborator: ["Collaborator 1", "Collaborator 2"],
         description: "Description for Project 1",
         link_video: "https://youtu.be/dK7a9rDgU3g?si=3DDhxZbAEm0E8745",
-        link_gambar: "asset/1 TOP.png",
+        link_gambar: "asset/top2.png",
         rank: '1'
     },
     {
@@ -14,7 +14,7 @@ const data_dummy = [
         collaborator: ["Collaborator 3", "Collaborator 4"],
         description: "Description for Project 2",
         link_video: "https://youtu.be/dK7a9rDgU3g?si=3DDhxZbAEm0E8745",
-        link_gambar: "asset/1 TOP.png",
+        link_gambar: "asset/top2.png",
         rank: '2'
     },
     {
@@ -23,7 +23,7 @@ const data_dummy = [
         collaborator: ["Collaborator 5", "Collaborator 6"],
         description: "Description for Project 3",
         link_video: "https://youtu.be/dK7a9rDgU3g?si=3DDhxZbAEm0E8745",
-        link_gambar: "asset/1 TOP.png",
+        link_gambar: "asset/top3.png",
         rank: '3'
     },
     {
@@ -32,7 +32,7 @@ const data_dummy = [
         collaborator: ["Collaborator 7", "Collaborator 8"],
         description: "Description for Project 4",
         link_video: "https://youtu.be/dK7a9rDgU3g?si=3DDhxZbAEm0E8745",
-        link_gambar: "asset/1 TOP.png",
+        link_gambar: "asset/top4.png",
         rank: '4'
     },
     {
@@ -41,7 +41,7 @@ const data_dummy = [
         collaborator: ["Collaborator 9", "Collaborator 10", "Collaborator 10", "Collaborator 10", "Collaborator 10", "Collaborator 10"],
         description: "Description for Project 5",
         link_video: "https://youtu.be/dK7a9rDgU3g?si=3DDhxZbAEm0E8745",
-        link_gambar: "asset/1 TOP.png",
+        link_gambar: "asset/top5.png",
         rank: '5'
     },
     {
@@ -50,7 +50,7 @@ const data_dummy = [
         collaborator: ["Collaborator 11", "Collaborator 12"],
         description: "Description for Project 6",
         link_video: "https://youtu.be/dK7a9rDgU3g?si=3DDhxZbAEm0E8745",
-        link_gambar: "asset/1 TOP.png",
+        link_gambar: "asset/top6.png",
         rank: '6'
     },
     {
@@ -59,7 +59,7 @@ const data_dummy = [
         collaborator: ["Collaborator 13", "Collaborator 14"],
         description: "Description for Project 7",
         link_video: "https://youtu.be/dK7a9rDgU3g?si=3DDhxZbAEm0E8745",
-        link_gambar: "asset/1 TOP.png",
+        link_gambar: "asset/top7.png",
         rank: '7'
     },
     {
@@ -68,7 +68,7 @@ const data_dummy = [
         collaborator: ["Collaborator 15", "Collaborator 16"],
         description: "Description for Project 8",
         link_video: "https://youtu.be/dK7a9rDgU3g?si=3DDhxZbAEm0E8745",
-        link_gambar: "asset/1 TOP.png",
+        link_gambar: "asset/top8.png",
         rank: '8'
     },
     {
@@ -77,7 +77,7 @@ const data_dummy = [
         collaborator: ["Collaborator 17", "Collaborator 18"],
         description: "Description for Project 9",
         link_video: "https://youtu.be/dK7a9rDgU3g?si=3DDhxZbAEm0E8745",
-        link_gambar: "asset/1 TOP.png",
+        link_gambar: "asset/top9.png",
         rank: '9'
     },
     {
@@ -86,7 +86,7 @@ const data_dummy = [
         collaborator: ["Collaborator 19", "Collaborator 20"],
         description: "Description for Project 10",
         link_video: "https://youtu.be/dK7a9rDgU3g?si=3DDhxZbAEm0E8745",
-        link_gambar: "asset/1 TOP.png",
+        link_gambar: "asset/top10.png",
         rank: '10'
     }
 ]
