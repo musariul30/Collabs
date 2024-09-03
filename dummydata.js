9dba30106e0ecf98cf5c9a14ce42c1902f5dea1f
arnellahayya <arnellahayya25@gmail.com>
2024-09-03 15:14:56 +0700

    update top10

--- dummydata.js
+++ dummydata.js
@@ -18,20 +18,20 @@ const data_dummy = [
         rank: '2'
     },
     {
-        name: "Left and Right",
-        owner: "Charlie Puth",
-        collaborator: ["Jung Kook of BTS"],
+        name: "3D",
+        owner: "Jung Kook 정국",
+        collaborator: ["Jack Harlow"],
         description: "Description for Project 3",
         link_video: "https://youtu.be/mHNCM-YALSA?si=_4Tp_PvD1wSNg0qv",
         link_gambar: "asset/top3.png",
         rank: '3'
     },
     {
-        name: "Project 4",
+        name: "Bruno Mars",
         owner: "Owner D",
-        collaborator: ["Collaborator 7", "Collaborator 8"],
+        collaborator: ["Anderson .Paak", "Silk Sonic"],
         description: "Description for Project 4",
-        link_video: "https://youtu.be/dK7a9rDgU3g?si=3DDhxZbAEm0E8745",
+        link_video: "https://youtu.be/adLGHcj_fmA?si=yvUz3W8sCjjFvjpB",
         link_gambar: "asset/top4.png",
         rank: '4'
     },
@@ -45,47 +45,47 @@ const data_dummy = [
         rank: '5'
     },
     {
-        name: "Project 6",
-        owner: "Owner F",
-        collaborator: ["Collaborator 11", "Collaborator 12"],
+        name: "Peaches",
+        owner: "Justin Bieber",
+        collaborator: ["Daniel Caesar"],
         description: "Description for Project 6",
-        link_video: "https://youtu.be/dK7a9rDgU3g?si=3DDhxZbAEm0E8745",
+        link_video: "https://youtu.be/tQ0yjYUFKAE?si=ENuxXlZnmL0VN5vv",
         link_gambar: "asset/top6.png",
         rank: '6'
     },
     {
-        name: "Project 7",
-        owner: "Owner G",
-        collaborator: ["Collaborator 13", "Collaborator 14"],
+        name: "Beautiful Mitakes",
+        owner: "Maroon 5",
+        collaborator: ["Megan Thee Stallion"],
         description: "Description for Project 7",
-        link_video: "https://youtu.be/dK7a9rDgU3g?si=3DDhxZbAEm0E8745",
+        link_video: "https://youtu.be/BSzSn-PRdtI?si=aOZ24TxdWDmGhd5X",
         link_gambar: "asset/top7.png",
         rank: '7'
     },
     {
-        name: "Project 8",
-        owner: "Owner H",
-        collaborator: ["Collaborator 15", "Collaborator 16"],
+        name: "Rewrite The Stars",
+        owner: "James Arthur",
+        collaborator: ["Anne Marie"],
         description: "Description for Project 8",
-        link_video: "https://youtu.be/dK7a9rDgU3g?si=3DDhxZbAEm0E8745",
+        link_video: "https://youtu.be/pRfmrE0ToTo?si=pVO_Ug5byc4ZtVLq",
         link_gambar: "asset/top8.png",
         rank: '8'
     },
     {
-        name: "Project 9",
-        owner: "Owner I",
-        collaborator: ["Collaborator 17", "Collaborator 18"],
+        name: "Heaven",
+        owner: "Calum Scott",
+        collaborator: ["Lyodra"],
         description: "Description for Project 9",
-        link_video: "https://youtu.be/dK7a9rDgU3g?si=3DDhxZbAEm0E8745",
+        link_video: "https://youtu.be/NlG_aFs7gdg?si=nhU-8BKE6VRYeXTR",
         link_gambar: "asset/top9.png",
         rank: '9'
     },
     {
-        name: "Project 10",
-        owner: "Owner J",
-        collaborator: ["Collaborator 19", "Collaborator 20"],
+        name: "Welcome Back",
+        owner: "Ali Gatie",
+        collaborator: ["Alessia Cara"],
         description: "Description for Project 10",
-        link_video: "https://youtu.be/dK7a9rDgU3g?si=3DDhxZbAEm0E8745",
+        link_video: "https://youtu.be/KFF9ccipraw?si=gThMbVpp5swFTLvZ",
         link_gambar: "asset/top10.png",
         rank: '10'
     }
