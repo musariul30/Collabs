648e36a3b5638a24a50329f98b30b2d2a1503a06
arnellahayya <arnellahayya25@gmail.com>
2024-09-06 15:40:06 +0700

    up

--- js/dummydata-new.js
+++ js/dummydata-new.js
@@ -1,30 +1,30 @@
 const data_dummy_new = [
     {
-        name: "It Has To Be You",
-        owner: "YESUNG 예성",
-        collaborator: ["Super Junior"],
+        name: "Die With a Smile",
+        owner: "Bruno Mars",
+        collaborator: ["Lady Gaga"],
         description: "In the Super Show Spin Off: Halftime, Super Junior delivers a heartfelt performance of 'It Has To Be You', originally sung by Yesung for the Cinderella’s Stepsister OST. The group brings new depth to the emotional ballad, showcasing their vocal harmony while honoring the original rendition.",
-        link_video: "https://youtu.be/gmFUioBEJzs?si=fXXDs73YIUE4w_p-",
+        link_video: "https://youtu.be/kPa7bsKwL-c?si=p4ntdbTGKhj_O088",
         link_gambar: "../asset/new1.png",
-        rank: '1'
+        rank: '11'
     },
     {
-        name: "Floral sense",
-        owner: "YESUNG 예성",
-        collaborator: ["Winter of Aespa"],
+        name: "Fortnight",
+        owner: "Taylor Swift",
+        collaborator: ["Post Malone"],
         description: "Description for Project 2",
-        link_video: "https://youtu.be/dK7a9rDgU3g?si=dZyQDQz3lw9ZClUV",
+        link_video: "https://youtu.be/q3zqJs7JUCQ?si=8OlXN39dhAV8Ht_p",
         link_gambar: "../asset/new-1.png",
-        rank: '2'
+        rank: '12'
     },
     {
-        name: "It Has To Be You",
-        owner: "YESUNG 예성",
-        collaborator: ["Super Junior"],
+        name: "All My Life",
+        owner: "Lil Durk",
+        collaborator: ["J. Cole"],
         description: "In the Super Show Spin Off: Halftime, Super Junior delivers a heartfelt performance of 'It Has To Be You', originally sung by Yesung for the Cinderella’s Stepsister OST. The group brings new depth to the emotional ballad, showcasing their vocal harmony while honoring the original rendition.",
-        link_video: "https://youtu.be/gmFUioBEJzs?si=fXXDs73YIUE4w_p-",
+        link_video: "https://youtu.be/Z4N8lzKNfy4?si=2hbRTq5aARX0QeFn",
         link_gambar: "../asset/new3.png",
-        rank: '1'
+        rank: '13'
     },
     {
         name: "Floral sense",
@@ -33,7 +33,7 @@ const data_dummy_new = [
         description: "Description for Project 2",
         link_video: "https://youtu.be/dK7a9rDgU3g?si=dZyQDQz3lw9ZClUV",
         link_gambar: "../asset/new4.png",
-        rank: '2'
+        rank: '14'
     },
     {
         name: "It Has To Be You",
@@ -42,7 +42,7 @@ const data_dummy_new = [
         description: "In the Super Show Spin Off: Halftime, Super Junior delivers a heartfelt performance of 'It Has To Be You', originally sung by Yesung for the Cinderella’s Stepsister OST. The group brings new depth to the emotional ballad, showcasing their vocal harmony while honoring the original rendition.",
         link_video: "https://youtu.be/gmFUioBEJzs?si=fXXDs73YIUE4w_p-",
         link_gambar: "../asset/new5.png",
-        rank: '1'
+        rank: '15'
     },
     {
         name: "Floral sense",
@@ -51,7 +51,7 @@ const data_dummy_new = [
         description: "Description for Project 2",
         link_video: "https://youtu.be/dK7a9rDgU3g?si=dZyQDQz3lw9ZClUV",
         link_gambar: "../asset/new6.png",
-        rank: '2'
+        rank: '16'
     },
     {
         name: "It Has To Be You",
@@ -60,7 +60,7 @@ const data_dummy_new = [
         description: "In the Super Show Spin Off: Halftime, Super Junior delivers a heartfelt performance of 'It Has To Be You', originally sung by Yesung for the Cinderella’s Stepsister OST. The group brings new depth to the emotional ballad, showcasing their vocal harmony while honoring the original rendition.",
         link_video: "https://youtu.be/gmFUioBEJzs?si=fXXDs73YIUE4w_p-",
         link_gambar: "../asset/new7.png",
-        rank: '1'
+        rank: '17'
     },
     {
         name: "Floral sense",
@@ -69,25 +69,25 @@ const data_dummy_new = [
         description: "Description for Project 2",
         link_video: "https://youtu.be/dK7a9rDgU3g?si=dZyQDQz3lw9ZClUV",
         link_gambar: "../asset/new8.png",
-        rank: '2'
+        rank: '18'
     },
     {
-        name: "It Has To Be You",
-        owner: "YESUNG 예성",
-        collaborator: ["Super Junior"],
+        name: "Dientes",
+        owner: "J Balvin",
+        collaborator: ["USHER &  DJ Khaled"],
         description: "In the Super Show Spin Off: Halftime, Super Junior delivers a heartfelt performance of 'It Has To Be You', originally sung by Yesung for the Cinderella’s Stepsister OST. The group brings new depth to the emotional ballad, showcasing their vocal harmony while honoring the original rendition.",
-        link_video: "https://youtu.be/gmFUioBEJzs?si=fXXDs73YIUE4w_p-",
+        link_video: "https://youtu.be/gVPgwoXm5Rc?si=5np-x2H3N7-N7IXP",
         link_gambar: "../asset/new9.png",
-        rank: '1'
+        rank: '19'
     },
     {
-        name: "Floral sense",
-        owner: "YESUNG 예성",
-        collaborator: ["Winter of Aespa"],
+        name: "Why I Love You So",
+        owner: "Alok",
+        collaborator: ["James Hurr & Supafly"],
         description: "Description for Project 2",
-        link_video: "https://youtu.be/dK7a9rDgU3g?si=dZyQDQz3lw9ZClUV",
+        link_video: "https://youtu.be/iC_DzhHEDKI?si=7Dr9dN426-gEb_Cl",
         link_gambar: "../asset/new10.png",
-        rank: '2'
+        rank: '20'
     },
 ]
 
