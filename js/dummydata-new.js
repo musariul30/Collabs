e8f618647512cb08fe93de39cd7956a8315eae84
arnellahayya <arnellahayya25@gmail.com>
2024-09-06 14:42:55 +0700

    icon app

--- js/dummydata-new.js
+++ js/dummydata-new.js
@@ -5,7 +5,7 @@ const data_dummy_new = [
         collaborator: ["Super Junior"],
         description: "In the Super Show Spin Off: Halftime, Super Junior delivers a heartfelt performance of 'It Has To Be You', originally sung by Yesung for the Cinderella’s Stepsister OST. The group brings new depth to the emotional ballad, showcasing their vocal harmony while honoring the original rendition.",
         link_video: "https://youtu.be/gmFUioBEJzs?si=fXXDs73YIUE4w_p-",
-        link_gambar: "../asset/top2.png",
+        link_gambar: "../asset/new1.png",
         rank: '1'
     },
     {
@@ -14,7 +14,7 @@ const data_dummy_new = [
         collaborator: ["Winter of Aespa"],
         description: "Description for Project 2",
         link_video: "https://youtu.be/dK7a9rDgU3g?si=dZyQDQz3lw9ZClUV",
-        link_gambar: "../asset/top2.png",
+        link_gambar: "../asset/new-1.png",
         rank: '2'
     },
     {
@@ -23,7 +23,7 @@ const data_dummy_new = [
         collaborator: ["Super Junior"],
         description: "In the Super Show Spin Off: Halftime, Super Junior delivers a heartfelt performance of 'It Has To Be You', originally sung by Yesung for the Cinderella’s Stepsister OST. The group brings new depth to the emotional ballad, showcasing their vocal harmony while honoring the original rendition.",
         link_video: "https://youtu.be/gmFUioBEJzs?si=fXXDs73YIUE4w_p-",
-        link_gambar: "../asset/top2.png",
+        link_gambar: "../asset/new3.png",
         rank: '1'
     },
     {
@@ -32,7 +32,7 @@ const data_dummy_new = [
         collaborator: ["Winter of Aespa"],
         description: "Description for Project 2",
         link_video: "https://youtu.be/dK7a9rDgU3g?si=dZyQDQz3lw9ZClUV",
-        link_gambar: "../asset/top2.png",
+        link_gambar: "../asset/new4.png",
         rank: '2'
     },
     {
@@ -41,7 +41,7 @@ const data_dummy_new = [
         collaborator: ["Super Junior"],
         description: "In the Super Show Spin Off: Halftime, Super Junior delivers a heartfelt performance of 'It Has To Be You', originally sung by Yesung for the Cinderella’s Stepsister OST. The group brings new depth to the emotional ballad, showcasing their vocal harmony while honoring the original rendition.",
         link_video: "https://youtu.be/gmFUioBEJzs?si=fXXDs73YIUE4w_p-",
-        link_gambar: "../asset/top2.png",
+        link_gambar: "../asset/new5.png",
         rank: '1'
     },
     {
@@ -50,7 +50,7 @@ const data_dummy_new = [
         collaborator: ["Winter of Aespa"],
         description: "Description for Project 2",
         link_video: "https://youtu.be/dK7a9rDgU3g?si=dZyQDQz3lw9ZClUV",
-        link_gambar: "../asset/top2.png",
+        link_gambar: "../asset/new6.png",
         rank: '2'
     },
     {
@@ -59,7 +59,7 @@ const data_dummy_new = [
         collaborator: ["Super Junior"],
         description: "In the Super Show Spin Off: Halftime, Super Junior delivers a heartfelt performance of 'It Has To Be You', originally sung by Yesung for the Cinderella’s Stepsister OST. The group brings new depth to the emotional ballad, showcasing their vocal harmony while honoring the original rendition.",
         link_video: "https://youtu.be/gmFUioBEJzs?si=fXXDs73YIUE4w_p-",
-        link_gambar: "../asset/top2.png",
+        link_gambar: "../asset/new7.png",
         rank: '1'
     },
     {
@@ -68,7 +68,7 @@ const data_dummy_new = [
         collaborator: ["Winter of Aespa"],
         description: "Description for Project 2",
         link_video: "https://youtu.be/dK7a9rDgU3g?si=dZyQDQz3lw9ZClUV",
-        link_gambar: "../asset/top2.png",
+        link_gambar: "../asset/new8.png",
         rank: '2'
     },
     {
@@ -77,7 +77,7 @@ const data_dummy_new = [
         collaborator: ["Super Junior"],
         description: "In the Super Show Spin Off: Halftime, Super Junior delivers a heartfelt performance of 'It Has To Be You', originally sung by Yesung for the Cinderella’s Stepsister OST. The group brings new depth to the emotional ballad, showcasing their vocal harmony while honoring the original rendition.",
         link_video: "https://youtu.be/gmFUioBEJzs?si=fXXDs73YIUE4w_p-",
-        link_gambar: "../asset/top2.png",
+        link_gambar: "../asset/new9.png",
         rank: '1'
     },
     {
@@ -86,7 +86,7 @@ const data_dummy_new = [
         collaborator: ["Winter of Aespa"],
         description: "Description for Project 2",
         link_video: "https://youtu.be/dK7a9rDgU3g?si=dZyQDQz3lw9ZClUV",
-        link_gambar: "../asset/top2.png",
+        link_gambar: "../asset/new10.png",
         rank: '2'
     },
 ]
